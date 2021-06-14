adc1cddd2f126e55a85002749d705dad0b51cce1
abu sayeed <sayeed0209@gmail.com>
2021-06-14 12:42:55 +0200

    find user by id

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const port = 8000;
 const app = express();
 
 const User = require("./models/User");
-console.log(User);
 mongoose.connect("mongodb://localhost/userData", {
 	useNewUrlParser: true,
 	useUnifiedTopology: true,
@@ -19,14 +18,43 @@ app.listen(port, () => {
 
 // CREATE
 app.post("/users", (req, res) => {
-	// User.create()
+	User.create(
+		{
+			name: req.body.newData.name,
+			email: req.body.newData.email,
+			password: req.body.newData.password,
+		},
+		(err, data) => {
+			if (err) {
+				res.json({ success: false, message: err });
+			} else if (!data) {
+				res.json({ success: false, message: "Not Found" });
+			} else {
+				res.json({ success: true, data: data });
+			}
+		}
+	);
 });
 
 app
 	.route("/users/:id")
 	// READ
 	.get((req, res) => {
-		// User.findById()
+		User.findById(req.params.id, (err, data) => {
+			if (err) {
+				res.json({
+					success: false,
+					message: err,
+				});
+			} else if (!data) {
+				res.json({
+					sucesss: false,
+					message: "Not found!",
+				});
+			} else {
+				res.json({ success: true, data: data });
+			}
+		});
 	})
 	// UPDATE
 	.put((req, res) => {
